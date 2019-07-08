2cd089216e4f4b6c1a10191f0cc761afb27362f5
shu <shu@atsu-maru.co.jp>
2019-07-09 00:29:53 +0900

    ルーティング＆コントローラ

--- test1/routes/web.php
+++ test1/routes/web.php
@@ -14,3 +14,80 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+$html = <<<EOF
+<html>
+<head>
+<style>
+h1 {
+    font-size: 100pt;
+    text-align: center;
+    color: blue;
+}
+</style>
+</head>
+</html>
+<body>
+<h1>HELLOOOOO</h1>
+<p>this is example</p>
+</body>
+EOF;
+
+Route::get('hello', function()use($html){
+    return $html;
+});
+
+
+Route::get('helo/{msg1}/{msg2}', function($msg1, $msg2){
+
+$html1 = <<<EOF
+<html>
+<head>
+<style>
+h1 {
+    font-size: 100pt;
+    text-align: center;
+    color: blue;
+}
+</style>
+</head>
+</html>
+<body>
+<h1>HELLOOOOO</h1>
+<p>this is {$msg1} and {$msg2} example</p>
+</body>
+EOF;
+
+
+    return $html1;
+});
+
+
+Route::get('helo/{msg1?}', function($msg1='no message'){
+
+$html1 = <<<EOF
+<html>
+<head>
+<style>
+h1 {
+    font-size: 100pt;
+    text-align: center;
+    color: blue;
+}
+</style>
+</head>
+</html>
+<body>
+<h1>HELLOOOOO</h1>
+<p>this is {$msg1} and example</p>
+</body>
+EOF;
+
+
+    return $html1;
+});
+
+// Route::get('helllo/{id?}/{pass?}', 'HelloController@index');
+
+Route::get('hello', 'HelloController@index');
+Route::get('hello/other', 'HelloController@other');
